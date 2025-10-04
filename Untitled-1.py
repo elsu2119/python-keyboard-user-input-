4706d83952680dffbf7961a9ede57a6f7be10fce
elsu2119 <elsunegash576@gmail.com>
2025-10-04 19:08:54 +0300

    Initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+# Python program to take name, ID, and age from keyboard
+
+# Taking input from the user
+name = input("Enter your name: ")
+student_id = input("Enter your ID: ")
+age = input("Enter your age: ")
+
+# Displaying the input values
+print("\n--- User Details ---")
+print("Name:", name)
+print("ID:", student_id)
+print("Age:", age)
+e
